d28f3924d6d4293919aac027476c0ad9889128ae
Daniel Naylor <git@drnaylor.co.uk>
2013-02-06 21:56:12 +0000

    Adding MIT license blocks.

--- src/main/java/uk/co/drnaylor/mcmmopartyadmin/PartyAdmin.java
+++ src/main/java/uk/co/drnaylor/mcmmopartyadmin/PartyAdmin.java
@@ -1,3 +1,22 @@
+/* 
+ * Copyright (C) 2013 Dr Daniel R. Naylor
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+ * and associated documentation files (the "Software"), to deal in the Software without restriction, 
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+ * subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial 
+ * portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT 
+ * LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ **/
 package uk.co.drnaylor.mcmmopartyadmin;
 
 import org.bukkit.plugin.java.JavaPlugin;
@@ -42,7 +61,7 @@ public class PartyAdmin extends JavaPlugin {
 
         getCommand("pa").setExecutor(new PartyAdminCommand(this));
         getCommand("partyspy").setExecutor(new PartySpy(this));
-        this.getServer().getLogger().info("[mcMMO Party Admin] mcMMO Party Admin is now enabled.");
+        this.getServer().getLogger().info("[mcMMO Party Admin] mcMMO Party Admin " + _version + " is now enabled.");
     }
 
     @Override
